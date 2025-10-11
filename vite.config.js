3f9b785d74eeea895644f425d61ed144da0a5057
Jan Bukowski <janmaciej.bukowski@gmail.com>
2025-10-11 22:35:34 +0200

    bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/jadlospis/", // <- bardzo ważne
+  base: "./",
   plugins: [react()],
 });
